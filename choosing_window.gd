6bc71fa7f4bd6418b813f14eb616d1c6769d9a20
iamblobfish <asf2003@yandex.ru>
2024-02-29 20:30:44 +0200

    updates

--- choosing_window.gd
+++ choosing_window.gd
@@ -1,43 +1,41 @@
-extends PopupPanel
-signal close
+extends Panel
+signal btn3
 signal create
 signal delete
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	create.connect(_on_create_pressed)
+	delete.connect(_on_delete_pressed)
+	btn3.connect(_on_btn3_pressed)
+	#anchors_preset = PRESET_RIGHT_WIDE
 	hide()
-# Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-func show_add(lst):
-	print(lst)
-	for el in lst:
-		print(el)
-		print($VBoxContainer/ItemList.add_item(el))	
-	show()
-	
-	
-
-
-
 func _on_create_pressed():
+	print("Create click")
 	create.emit()
-	pass # Replace with function body.
-
-
-func _on_close_pressed():
-	close.emit()
-	hide()
-	
-
 
 
 func _on_delete_pressed():
 	delete.emit()
-	 # Replace with function body.
+
+func _on_btn3_pressed():
+	btn3.emit()
+
+func _on_visibility_changed():
+	print('fuck')
+
+
+func _on_forest_tile_2_show_window():
+	print('show vindow in window')
+	show()
+
+
+func _on_forest_tile_focus_exited():
+	hide()
+
